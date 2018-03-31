79f852074ccd43f58ed9dde4ec54841407c9476f
Vinod <jvinod@portworx.com>
2018-03-31 21:53:52 +0000

    handle trailing /

--- src/Protocols/NFS/tracker.c
+++ src/Protocols/NFS/tracker.c
@@ -11,9 +11,16 @@
 #include <errno.h>
 #include <assert.h>
 
+#include "log.h"
+
 #define TRACKER_PATH "/var/lib/osd/tracker"
 #define UNKNOWN_CLIENT "uc"
 
+#ifdef UNIT_TEST
+#define LogCrit fprintf
+#define LogWarn fprintf
+#define COMPONENT_NFSPROTO stderr
+#endif
 static void __tracker_canonicalize(
 	const char *client, 
 	const char *mount_name, 
@@ -22,19 +29,25 @@ static void __tracker_canonicalize(
 	snprintf(path, PATH_MAX, "%s/%s_%s", TRACKER_PATH, client, mount_name); 
 }
 
-static const char * __tracker_dirname(const char *path)
+static const char * __tracker_dirname(char *path)
 {
+	size_t len = strlen(path);
+
+	while (len > 1 && path[len - 1] == '/') {
+		path[len - 1] = '\0';
+		len--;
+	}
 	const char *p = strrchr(path, '/');
 	return p ? p + 1 : path;
 }
 
-static void __tracker_path(const char *mount_path, char out_path[PATH_MAX])
+static void __tracker_path(char *mount_path, char out_path[PATH_MAX])
 {
 	const char *mount_name = __tracker_dirname(mount_path);
 	snprintf(out_path, PATH_MAX, "%s/%s", TRACKER_PATH, mount_name); 
 }
 
-static int __tracker_find_client(const char *mount_path, char client[PATH_MAX])
+static int __tracker_find_client(char *mount_path, char client[PATH_MAX])
 {
 	DIR *dirp = NULL;
 	int ret = ENOENT;
@@ -43,7 +56,7 @@ static int __tracker_find_client(const char *mount_path, char client[PATH_MAX])
 
 	dirp = opendir(TRACKER_PATH);
 	if (dirp == NULL) {
-		fprintf(stderr, "Failed to open %s: %s\n", 
+		LogWarn(COMPONENT_NFSPROTO, "Failed to open %s: %s\n", 
 				TRACKER_PATH, strerror(errno));
 		ret = -errno;
 		goto done;
@@ -70,7 +83,7 @@ done:
 
 static int __tracker_client_path(
 	const char *client,
-	const char *mount_path,
+	char *mount_path,
 	char out_path[PATH_MAX])
 {
 	int ret = ENOENT;
@@ -80,7 +93,8 @@ static int __tracker_client_path(
 	assert(client);;
 
 	if (strrchr(client, '/') != NULL) {
-		fprintf(stderr, "Don't like clients with slash %s\n", client);
+		LogWarn(COMPONENT_NFSPROTO, 
+			"Don't like clients with slash %s\n", client);
 		ret = -1;
 		goto done;
 	}
@@ -93,7 +107,7 @@ done:
 }
 
 static int __tracker_rand_client_path(
-	const char *mount_path,
+	char *mount_path,
 	char out_path[PATH_MAX])
 {
 	char client[256];
@@ -107,31 +121,36 @@ static int __tracker_rand_client_path(
 	return exists;
 }
 
-int tracker_mount(const char *client, const char *mount_path) 
+int tracker_mount(const char *client, const char *mount_path_in) 
 {
-	char tracker_path[PATH_MAX], link_path[PATH_MAX];
+	char tracker_path[PATH_MAX], link_path[PATH_MAX], mount_path[PATH_MAX];
+
 	int fd = -1, ret = 0;
 
+	strncpy(mount_path, mount_path_in, PATH_MAX);
 	ret = client == NULL || strcmp(client, "") == 0 
 		? __tracker_rand_client_path(mount_path, link_path)
 		: !__tracker_client_path(client, mount_path, link_path);
 	if (ret != 0) {
-		fprintf(stderr, "Failed to create client mount path (%s, %s)\n",
-				(client ? : "NULL"), (mount_path ? : "NULL"));
+		LogWarn(COMPONENT_NFSPROTO, 
+			"Failed to create client mount path (%s, %s) %d\n",
+			(client ? : "NULL"), (mount_path ? : "NULL"), ret);
 		goto done;
 	}
 
 	__tracker_path(mount_path, tracker_path);
 	if ((fd = open(tracker_path, O_CREAT | O_RDWR, 0600)) < 0) {
-		fprintf(stderr, "Failed to open %s: %s\n", 
-				tracker_path, strerror(errno));
+		LogWarn(COMPONENT_NFSPROTO, 
+			"Failed to open mount path (%s, %s) %d\n",
+			(client ? : "NULL"), (mount_path ? : "NULL"), ret);
 		ret = -errno;
 		goto done;
 	}
 
 	if ((ret = link(tracker_path, link_path)) != 0) {
-		fprintf(stderr, "Failed to link %s->%s: %s\n", 
-				tracker_path, link_path, strerror(errno));
+		LogWarn(COMPONENT_NFSPROTO, 
+			"Failed to link %s->%s: %s\n", 
+			tracker_path, link_path, strerror(errno));
 		ret = -errno;
 		goto done;
 	}
@@ -142,41 +161,48 @@ done:
 	return ret;
 }
 
-int tracker_unmount(const char *client, const char *mount_path)
+int tracker_unmount(const char *client, const char *mount_path_in)
 {
 	char path[PATH_MAX];
+	char mount_path[PATH_MAX];
 	struct stat st;
 	int ret;
+	
+	strncpy(mount_path, mount_path_in, PATH_MAX);
 
 	ret = client == NULL || strcmp(client, "") == 0 
 		? __tracker_find_client(mount_path, path)
 		: __tracker_client_path(client, mount_path, path);
 
 	if (ret != 0) {
-		fprintf(stderr, "Failed to find client mount path (%s, %s)\n",
-				(client ? : "NULL"), (mount_path ? : "NULL"));
+		LogWarn(COMPONENT_NFSPROTO, 
+			"Failed to find client mount path (%s, %s)\n",
+			(client ? : "NULL"), (mount_path ? : "NULL"));
 		goto done;
 	}
 
 	if ((ret = unlink(path)) != 0) {
-		fprintf(stderr, "Failed to unlink %s: %s\n", 
-				path, strerror(errno));
+		LogWarn(COMPONENT_NFSPROTO, 
+			"Failed to unlink %s: %s\n", 
+			path, strerror(errno));
 		ret = -errno;
 		goto done;
 	}
 
 	__tracker_path(mount_path, path);
 	if ((ret = stat(path, &st)) != 0) {
-		fprintf(stderr, "Failed to stat %s: %s\n", 
-				path, strerror(errno));
+		LogWarn(COMPONENT_NFSPROTO, 
+			"Failed to stat %s: %s\n", 
+			path, strerror(errno));
 		ret = -errno;
 		goto done;
 	}
 
 	if (st.st_nlink == 1) {
 		if ((ret = unlink(path)) != 0) {
-			fprintf(stderr, "Failed to unlink %s: %s\n", 
-					path, strerror(errno));
+			LogWarn(COMPONENT_NFSPROTO, 
+				"Failed to unlink %s: %s\n", 
+				path, strerror(errno));
 			ret = -errno;
 			goto done;
 		}
@@ -189,7 +215,9 @@ done:
 int tracker_init()
 {
 	if (mkdir(TRACKER_PATH, 0777) && (errno != EEXIST)) {
-		perror("Could not create NFS export directory.");
+		LogCrit(COMPONENT_NFSPROTO, 
+			"Could not create NFS export directory %s: %s",
+			 TRACKER_PATH, strerror(errno));
 		return -errno;
 	}
 	return 0;
@@ -204,6 +232,8 @@ int main()
 	assert(tracker_init() == 0);;
 	__tracker_path("/foo", tracker_path);
 
+	tracker_mount(NULL, "/foo///");
+	tracker_mount(NULL, "/foo/");
 	tracker_mount(NULL, "/foo");
 	assert(stat(tracker_path, &st) == 0);
 
@@ -222,6 +252,8 @@ int main()
 	tracker_unmount(NULL, "/foo");
 	assert(stat(tracker_path, &st) == 0);
 
+	tracker_unmount(NULL, "/foo");
+	tracker_unmount(NULL, "/foo");
 	tracker_unmount("c1", "/foo");
 	assert(stat(tracker_path, &st) == 0);
 
